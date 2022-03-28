c9d0ed11a2698c0290f6a1466066f8fa3fec985b
Karunakar Mishra <42409280+Karumishra@users.noreply.github.com>
2022-03-28 17:15:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Rest from './component/Rest';
+
+function App() {
+  return (
+    <Rest />
+  );
+}
+
+export default App;
---
+++ src/component/menuAPI.js
@@ -0,0 +1,95 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "If you love Maggi, Just go for it!",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "aalu pakoda",
+      category: "snacks",
+      price: "20₹",
+      description:
+        "If you love Aalu Pakoda, Just go for it! ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "If you love Corn, Just go for it! ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chhola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "If you love Chhola, Just go for it!",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "snacks",
+      price: "80₹",
+      description:
+        "If you love Pizza, Just go for it!",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "If you love Non-Veg, Just go for it!",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "snacks",
+      price: "60₹",
+      description:
+        "If you love Sweets, Just go for it!",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "If you love Rajma Rice, Just go for it!",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "snacks",
+      price: "10₹",
+      description:
+        "If you love Samosa, Just go for it! ",
+    },
+    {
+        id: 10,
+        image: "../images/vegthali.jpg",
+        name: "Veg Thali",
+        category: "dinner",
+        price: "100₹",
+        description:
+          "If you love Veg, Just go for it!",
+      },
+  ];
+  
+  export default Menu;
